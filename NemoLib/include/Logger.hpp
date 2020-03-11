d67d557495763fd317574202f565ba08eb1a8e67
nickrohde <rohden@cwu.edu>
2020-03-11 02:16:40 -0700

    timestamp for logger

--- NemoLib/include/Logger.hpp
+++ NemoLib/include/Logger.hpp
@@ -6,13 +6,18 @@
 #include <mutex>
 #include <iostream>
 
+#include "Utility.hpp"
+
 static std::mutex mtx_cout;
 
 // Asynchronous output
 struct Logger
 {
     std::unique_lock<std::mutex> lk;
-    Logger() : lk(std::unique_lock<std::mutex>(mtx_cout)) {}
+    Logger() : lk(std::unique_lock<std::mutex>(mtx_cout)) 
+    {
+        put_time_stamp(std::cerr);
+    }
 
     template<typename T>
     Logger& operator<<(const T& _t)

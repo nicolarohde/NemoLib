af84351331c9c26d993b482f84b2ca392bfb20b9
nickrohde <rohden@cwu.edu>
2020-04-27 23:39:28 -0700

    changed how nemo collections are compared

--- NemoLib/cmp_nemo.py
+++ NemoLib/cmp_nemo.py
@@ -1,6 +1,13 @@
 from ast import literal_eval
 from sys import argv
-from bisect import insort
+from bisect import insort, bisect_left
+
+def binary_search(a, x):
+    'Locate the leftmost value exactly equal to x'
+    i = bisect_left(a, x)
+    if i != len(a) and a[i] == x:
+        return i
+    return -1
 
 
 def get_line_no_comment(f):
@@ -19,10 +26,12 @@ def get_line_pair(f):
         if not l2:
             break
 
-        yield l1, l2
+        yield l1.rstrip('\n'), l2.rstrip('\n')
 
 
-def get_sorted_motifs(f, motif_dict):
+def get_sorted_motifs(f):
+    motif_dict = {}
+
     for l1, l2 in get_line_pair(f):
         motif = literal_eval(l2)
         motif.sort()
@@ -32,18 +41,36 @@ def get_sorted_motifs(f, motif_dict):
         else:
             insort(motif_dict[l1], motif)
 
+    return motif_dict
+
 
 def print_mismatches(expected, actual):
     if len(expected) != len(actual):
         print(f"Mismatch in length: expected = {len(expected)} actual = {len(actual)}")
     
     if expected.keys() != actual.keys():
-        print(f"Mismatch in keys: expected = {expected.keys()} actual = {actual.keys()}")
+        print(f"Mismatch in keys: expected = {list(expected.keys())} actual = {list(actual.keys())}")
 
-    for expct, actl in zip(expected, actual):
-        for e, a in zip(expct, actl):
-            if e != a:
-                print(f"Mismatch in motif: expected = {e} actual = {a}")
+    wrong_motifs = {}
+
+    for k in expected.keys():
+        if k not in actual:
+            print(f"The key {k} was NOT present in actual mapping but was expected to be")
+            continue
+
+        for expected_motif in expected[k]:
+            if binary_search(actual[k], expected_motif) == -1:
+                if k in wrong_motifs:
+                    wrong_motifs[k] += 1
+                else:
+                    wrong_motifs[k] = 1
+
+    for k in actual.keys():
+        if k not in expected:
+            print(f"The key {k} was present in actual mapping but was NOT expected to be")
+
+    for k, v in wrong_motifs.items():
+        print(f"The key {k} contained {v} unexpected motifs a {float(v) / float(len(actual[k])) * 100}% mismatch")
 
 
 def main():
@@ -51,10 +78,10 @@ def main():
     actual_motifs = {}
 
     with open(argv[1]) as correct_nemo_file:
-        get_sorted_motifs(correct_nemo_file, expected_motifs)
+        expected_motifs = get_sorted_motifs(correct_nemo_file)
 
     with open(argv[2]) as new_nemo_file:
-        get_sorted_motifs(new_nemo_file, actual_motifs)
+        actual_motifs = get_sorted_motifs(new_nemo_file)
 
     print_mismatches(expected_motifs, actual_motifs)
 
